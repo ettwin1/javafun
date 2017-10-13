b363bff2a3c16cb00a90f33d0005387446418173
ettwin1 <ethanthomasalvi@gmail.com>
2017-10-13 09:53:35 -0600

    Second Commit for this file
    So... I changed the file and now I'm trying to commit this new file and
    see how it works, and I think I'm getting it so yeah

--- GitTester/src/GitTest.java
+++ GitTester/src/GitTest.java
@@ -3,6 +3,7 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		System.out.println("HEELLLLO HELLO HEELLOOOOOO");
+		System.out.println("I have changed this file..... MUAHAHAHAHA");
 	}
 
 }
