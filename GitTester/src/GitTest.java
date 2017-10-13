9030960db4fb6fa2aec32720e498269ad11a5ff1
ettwin1 <ethanthomasalvi@gmail.com>
2017-10-13 09:55:29 -0600

    Third Attempt
    If you haven't already you should, like, look at the code and stuff bro

--- GitTester/src/GitTest.java
+++ GitTester/src/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("HEELLLLO HELLO HEELLOOOOOO");
 		System.out.println("I have changed this file..... MUAHAHAHAHA");
+		System.out.println("Don't worry, I'll change this file in a GOOD way, unlike the bad way Line 6 demonstrates...");
 	}
 
 }
