9ff18119f0b4e810f60aae8c90af38d733ea3acf
ettwin1 <ethanthomasalvi@gmail.com>
2017-10-13 10:07:59 -0600

    Ze fourth Commit
    No need to worry, I have my code under control

--- GitTester/src/GitTest.java
+++ GitTester/src/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
 		System.out.println("HEELLLLO HELLO HEELLOOOOOO");
 		System.out.println("I have changed this file..... MUAHAHAHAHA");
 		System.out.println("Don't worry, I'll change this file in a GOOD way, unlike the bad way Line 6 demonstrates...");
+		System.out.println("You've made a BIG mistake messing with me.... THIS MEANS WAR!");
 	}
 
 }
